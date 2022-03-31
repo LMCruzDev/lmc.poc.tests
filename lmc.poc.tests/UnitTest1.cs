a679e4045aa63b5335e4ceab50eb9a55ca5b240d
lucas.cruz_ceracare <lucas.cruz@ceracare.co.uk>
2022-03-31 10:46:07 +0100

    Add project files.

---
+++ lmc.poc.tests/UnitTest1.cs
@@ -0,0 +1,74 @@
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using FluentAssertions;
+using TinyCsvParser;
+using TinyCsvParser.Mapping;
+using Xunit;
+
+namespace lmc.poc.tests
+{
+    public class UnitTest1
+    {
+        private readonly Calculator calculator;
+
+        public UnitTest1()
+        {
+            calculator = new Calculator();
+        }
+
+        public static IEnumerable<object[]> GetExcelData()
+        {
+            CsvParserOptions csvParserOptions = new CsvParserOptions(true, ',');
+            CsvCalculationDataMapping csvMapper = new CsvCalculationDataMapping();
+            CsvParser<CsvCalculationData> csvParser = new CsvParser<CsvCalculationData>(csvParserOptions, csvMapper);
+            var result = csvParser
+                .ReadFromFile(@"Teste.csv", Encoding.ASCII)
+                .ToList();
+
+            return result.Select(x => new object[]
+            {
+                x.Result.N1,
+                x.Result.N2,
+                x.Result.N3,
+                x.Result.Total,
+
+            });
+        }
+
+        [Theory]
+        [MemberData(nameof(GetExcelData))]
+        public void ExcelXlsTests(
+            int n1, 
+            int n2, 
+            int n3, 
+            int total)
+        {
+            // Act
+            var result = calculator.Sum(n1, n2, n3);
+
+            // Assert
+            result.Should().Be(total);
+        }
+
+        public class CsvCalculationData
+        {
+            public int N1 { get; set; }
+            public int N2 { get; set; }
+            public int N3 { get; set; }
+            public int Total { get; set; }
+        }
+
+        public class CsvCalculationDataMapping : CsvMapping<CsvCalculationData>
+        {
+            public CsvCalculationDataMapping():base()
+            {
+                MapProperty(0, x => x.N1);
+                MapProperty(1, x => x.N2);
+                MapProperty(2, x => x.N3);
+                MapProperty(3, x => x.Total);
+            }
+        }
+    }
+}
---
+++ lmc.poc/Calculator.cs
@@ -0,0 +1,7 @@
+﻿namespace lmc.poc
+{
+    public class Calculator
+    {
+        public int Sum(int n1, int n2, int n3) => n1 + n2 + n3;
+    }
+}
